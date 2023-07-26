7a87dd3ef18fbe188e14eef17362f0ff0e725bcf
polaiah123 <54682944+polaiah123@users.noreply.github.com>
2023-07-26 20:23:15 +0530

    Add files via upload

---
+++ src/components/CarSearch.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
+
+function CarSearch() {
+const  dispatch = useDispatch();
+const searchTerm = useSelector((state)=>{
+  return state.cars.searchTerm;
+  });
+
+const handleSearchTermChange = (event) => {
+  dispatch(changeSearchTerm(event.target.value));
+};
+
+
+
+return (
+  <div className='list-header'>
+    <h3 className='title is-3'> My Car </h3>
+      <div className='search field is-horizontal'>
+          <label className='label'> Search
+            <input 
+            className='input'
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+            />
+        </label>
+      </div>
+  </div>
+  );
+}
+
+export default CarSearch;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+import 'bulma/css/bulma.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
